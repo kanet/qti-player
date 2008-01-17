e8f1c2d713d374fd8b1c176dc49ec0f40a668a39
klangner <klangner@17d68a0d-d843-0410-9017-7787d5464fed>
2008-01-17 12:15:55 +0000

    First version of the start page

--- aplayer/src/pl/klangner/qtiplayer/StartView.java
+++ aplayer/src/pl/klangner/qtiplayer/StartView.java
@@ -2,6 +2,7 @@ package pl.klangner.qtiplayer;
 
 import pl.klangner.qtiplayer.model.Assessment;
 import android.content.Context;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -19,13 +20,23 @@ public class StartView extends LinearLayout {
 	// --------------------------------------------------------------------------
 	private void createView() {
 
-		Context context = getContext();
-		
-		TextView	about_view = new TextView(context);
-		about_view.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+		Context 			context = getContext();
+		TextView			about_view = new TextView(context);
+		Button				start_button = new Button(context);
+		LayoutParams	lp;
+
+		setOrientation(VERTICAL);
+		lp = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		lp.gravity = 1;
+		about_view.setLayoutParams(lp);
+		about_view.setPadding(10, 10, 10, 10);
 		about_view.setText(assessment.getDescription());
 		
+		start_button.setText(R.string.start);
+		start_button.setLayoutParams(lp);
+		
 		addView(about_view);
+		addView(start_button);
 	}
 
 	// --------------------------------------------------------------------------
