90791b9c7b68c6a61d5214ea399cb56368a0e4da
klangner <klangner@17d68a0d-d843-0410-9017-7787d5464fed>
2009-10-06 14:20:35 +0000

    @ different ways to build TextWidget (static and interactive)

--- qtiplayer/src/com/klangner/qtiplayer/client/widget/TextWidget.java
+++ qtiplayer/src/com/klangner/qtiplayer/client/widget/TextWidget.java
@@ -2,6 +2,8 @@ package com.klangner.qtiplayer.client.widget;
 
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.xml.client.Element;
 
 public class TextWidget extends Composite{
@@ -11,11 +13,29 @@ public class TextWidget extends Composite{
 	 * @param node
 	 */
 	public TextWidget(Element node){
+	
+		Widget view;
+		
+		if(node.getElementsByTagName("inlineChoiceInteraction").getLength() > 0){
+			view = new Label("inline choice");
+		}
+		else{
+			view = staticTextView(node);
+		}
+
+		initWidget(view);
+	}
+	
+	/**
+	 * Create widget with static text
+	 */
+	private Widget staticTextView(Element node){
+		
 		HTML htmlLabel;
 		
 		htmlLabel = new HTML(node.toString());
 		htmlLabel.setStyleName("qp-text-module");
-		initWidget(htmlLabel);
+		return htmlLabel;
 	}
 	
 }
