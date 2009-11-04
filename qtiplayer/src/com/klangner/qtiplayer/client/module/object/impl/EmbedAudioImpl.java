4901b290d4d5f70eb704beb4b98a8ce14bf0b36d
klangner <klangner@17d68a0d-d843-0410-9017-7787d5464fed>
2009-11-04 11:37:09 +0000

    Fixed issue 59

---
+++ qtiplayer/src/com/klangner/qtiplayer/client/module/object/impl/EmbedAudioImpl.java
@@ -0,0 +1,11 @@
+package com.klangner.qtiplayer.client.module.object.impl;
+
+
+
+public class EmbedAudioImpl implements AudioImpl{
+
+  public String getHTML(String src){
+
+    return "<embed src='" + src + "' autostart='false' controller='true'></embed>";
+  }
+}
--- qtiplayer/src/com/klangner/qtiplayer/client/module/object/impl/LinkAudioImpl.java
+++
@@ -1,9 +0,0 @@
-package com.klangner.qtiplayer.client.module.object.impl;
-
-
-public class LinkAudioImpl implements AudioImpl{
-
-  public String getHTML(String src){
-    return "<a href='" + src + "'>Play</a>";
-  }
-}
