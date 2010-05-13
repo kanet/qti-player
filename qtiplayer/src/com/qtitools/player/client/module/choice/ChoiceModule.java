4095e917e9a2a7647d3e756f36e53ea7415c0e36
rafal.rybacki <rafal.rybacki@17d68a0d-d843-0410-9017-7787d5464fed>
2010-05-13 14:33:03 +0000

    bug 31458 checkbox response update

--- qtiplayer/src/com/qtitools/player/client/module/choice/ChoiceModule.java
+++ qtiplayer/src/com/qtitools/player/client/module/choice/ChoiceModule.java
@@ -231,6 +231,7 @@ public class ChoiceModule extends Composite implements IInteractionModule {
 			if (targertIsButton){
 				for (SimpleChoice currSC:interactionElements){
 					if (currSC.getInputId().compareTo(lastSelectedId) == 0){
+						currSC.setSelected(!currSC.isSelected());
 						currSC.showFeedback(!currSC.isSelected(), response.correctAnswers.contains(currSC.getIdentifier()));
 						continue;
 					}
