06a2cacd18b40a7ed030d125b403716da8917d10
rafal.rybacki <rafal.rybacki@17d68a0d-d843-0410-9017-7787d5464fed>
2010-07-15 07:22:20 +0000

    style controller files

---
+++ qtiplayer/src/com/qtitools/player/client/controller/style/StyleLinkSocket.java
@@ -0,0 +1,8 @@
+package com.qtitools.player.client.controller.style;
+
+import java.util.Vector;
+
+public interface StyleLinkSocket {
+
+	public Vector<String> getStyleLinksForUserAgent(String ua);
+}
