8c5e3d860894b2ab2d4c8f807abf1916f854d62b
rafal.rybacki <rafal.rybacki@17d68a0d-d843-0410-9017-7787d5464fed>
2011-01-12 12:16:30 +0000

    create config file

---
+++ qtiplayer/war/config.js
@@ -0,0 +1,9 @@
+if (window.QtiPlayer == null)
+	window.QtiPlayer = {};
+	
+window.QtiPlayer.flowoptions = {
+	SHOW_TOC: true,
+	SHOW_SUMMARY: true,
+	ITEMS_DISPLAY_MODE: "ONE",
+	SHOW_CHECK: false
+}
